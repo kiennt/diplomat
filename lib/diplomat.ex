6fa8f209ae30759b69b5b06e8b5ce1a0db591a35
Phil Burrows <github@philburrows.com>
2016-04-27 22:00:21 -0500

    this doesn't work, but it sets us up to support v1beta3

--- lib/diplomat.ex
+++ lib/diplomat.ex
@@ -1,6 +1,6 @@
 defmodule Diplomat do
   defmodule Proto do
-    use Protobuf, from: Path.expand("datastore_v1.proto", __DIR__)
-    # use Protobuf, from: Path.expand("datastore_v1beta3.proto", __DIR__)
+    # use Protobuf, from: Path.expand("datastore_v1.proto", __DIR__)
+    use Protobuf, from: Path.expand("datastore_v1beta3.proto", __DIR__)
   end
 end
